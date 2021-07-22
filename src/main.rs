75ceddd0abee06e4c6c9913cb6e3c3773a7dbf70
ge75maq <ge75maq@mytum.de>
2021-07-22 11:36:25 +0200

    Guessing game in Rust
     - random number generation
     - reading user input and parsing
     - simple error handling
     - simple loops

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Guess the number!");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1..100);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+            Ordering::Greater => println!("Too big!")
+        }
+    }
+
+
 }
