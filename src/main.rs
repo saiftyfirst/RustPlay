f00a49e270f9c28add7cafd866be11aa90a249d3
ge75maq <ge75maq@mytum.de>
2021-07-23 10:04:51 +0200

    Functions

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,11 @@
 
 fn main() {
 
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let x = plus_one(5);
+    println!("Value of x: {}", x);
 
-    let (_x, y, _z) = tup;
+}
 
-    println!("The value of y is: {}", y);
-
-    println!("The value of x is: {}", tup.0);
-
-
-    // arrays
-    let months = ["January", "February", "March", "April"];
-    println!("2nd month is: {}", months[1]);
-
-    let a: [i32; 5] = [1, 2, 3, 4, 5];
-    println!("Length of array is: {}", a.len());
-
-    let b: [i32; 5] = [3; 5];
-    println!("Length of array is: {}", b.len());
-    let mut i = 0;
-    loop {
-        match i < b.len() {
-            true => {
-                println!("Element i: {}", b[i]);
-                i += 1;
-            }
-            false => break
-        }
-    }
-
-
-
-
+fn plus_one(x: i32) -> i32 {
+    x + 1 // Adding a semicolon means that the expression value is not returned
 }
