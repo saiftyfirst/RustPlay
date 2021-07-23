19c9d40d58a1c0fa2a24b1da0b3787bd6edf3f65
ge75maq <ge75maq@mytum.de>
2021-07-23 18:30:09 +0200

    Slices

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,24 @@
 // use std::cmp::Ordering;
 
 fn main() {
-    let reference_to_nothing = dangle();
+    let mut s = String::from("hello world");
+
+    let first = get_word(&s);
+
+    println!("The first word is: {}", first);
+
+    s.clear();
+
 }
 
-fn dangle() -> String {
-    let s = String::from("Hello");
-    s
+fn get_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
