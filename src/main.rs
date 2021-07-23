81c6ec9b955a1bd457f97d8dc4983ea79052c28d
ge75maq <ge75maq@mytum.de>
2021-07-23 19:56:04 +0200

    Impl blocks, methods vs functions

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,40 @@ struct Rectangle {
     height: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
+}
+
 fn main() {
 
-    let rect_one = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
-        height: 50
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
     };
 
-    println!(
-        "The rectangle is {:?}",
-        rect_one
-    );
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
-fn area(rect: Rectangle) -> u32 {
-    rect.width * rect.height
-}
 
