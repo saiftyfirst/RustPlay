4023c32238b800e7d52e5bdbd8ac0803b0d4b9cb
ge75maq <ge75maq@mytum.de>
2021-07-23 20:55:29 +0200

    if-let as a less comprehensive match

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,9 @@
-use rand::distributions::Open01;
-
-#[derive(Debug)]
-enum UsState {
-    Alaska,
-    NewYork,
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState)
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        }
-    }
-
-}
-
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i+1)
-    }
-}
-
 fn main() {
 
-    // value_in_cents(Coin::Penny);
-    // value_in_cents(Coin::Quarter(UsState::Alaska));
+    let some_u8 = Some(3u8);
 
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-
-    let some_u8_value = 1;
-    match some_u8_value {
-        1 => println!("one"),
-        2 => println!("two"),
-        3 => println!("three"),
-        _ => ()
+    if let Some(3) = some_u8 {
+        println!("three");
     }
 
 }
