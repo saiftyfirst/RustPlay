ca5b7db838fb14befa87842395bb893c96bf99ff
ge75maq <ge75maq@mytum.de>
2021-07-23 18:09:27 +0200

    References

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,10 @@
 // use std::cmp::Ordering;
 
 fn main() {
+    let reference_to_nothing = dangle();
+}
 
-    for number in 1..4 {
-        println!("{}", number);
-    }
-
-    for number in (1..5).rev() {
-        println!("{}", number);
-    }
-
-    let a = [10, 20, 30, 40, 50];
-    for element in a.iter().rev() {
-        println!("this value is: {}", element);
-    }
-
-
+fn dangle() -> String {
+    let s = String::from("Hello");
+    s
 }
