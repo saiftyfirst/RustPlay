7b19b60ce0fef696be303dd40dda3ba068284521
ge75maq <ge75maq@mytum.de>
2021-07-23 20:22:30 +0200

    Enums

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,64 @@
+/* ENUMS v1
+enum IpAddrKind {
+    V4,
+    V6
+}
+// ENUMS v1
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String
+}
+*/
 
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String)
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
+// struct Ipv4Addr {
+//     // --snip--
+// }
+//
+// struct Ipv6Addr {
+//     // --snip--
+// }
+//
+// enum IpAddr {
+//     V4(Ipv4Addr),
+//     V6(Ipv6Addr),
+// }
 
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
 
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            height: size
-        }
+impl Message {
+    fn call(&self) {
     }
 }
 
 fn main() {
 
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
+    /*    ENUMS v1
+    let home = IpAddrKind {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1")
     };
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    let loopback = IpAddrKind {
+        kind: IpAddrKind::V6,
+        address: String::from("::1")
+    };*/
+
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
 }
 
 
